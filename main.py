6767e6f0c4319c31c189007942fa50d13e50baaa
Piotr Kurczab <piotr.kurczab2@student.urk.edu.pl>
2024-06-12 16:39:39 +0200

    Update name

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Database Manager")
+        self.setWindowTitle("Aplikacja bazodanowa")
         self.resize(800, 600)
 
         self.db = Database()
